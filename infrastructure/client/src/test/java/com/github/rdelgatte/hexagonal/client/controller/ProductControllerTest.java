ecb0e9584789399305bfe13ec8f99a83f6250ed2
Remi Delgatte <remi.delgatte@decathlon.com>
2018-10-18 16:17:11 +0200

    Setup initial architecture

---
+++ infrastructure/client/src/test/java/com/github/rdelgatte/hexagonal/client/controller/ProductControllerTest.java
@@ -0,0 +1,47 @@
+package com.github.rdelgatte.hexagonal.client.controller;
+
+
+import static io.vavr.API.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.github.rdelgatte.hexagonal.product.api.ProductService;
+import com.github.rdelgatte.hexagonal.product.domain.Product;
+import java.util.UUID;
+import name.falgout.jeffrey.testing.junit5.MockitoExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+@ExtendWith(MockitoExtension.class)
+class ProductControllerTest {
+
+  private Product product;
+  private ProductController cut;
+  @Mock
+  private ProductService productServiceMock;
+
+  @BeforeEach
+  void setUp() {
+    cut = new ProductController(productServiceMock);
+    product = new Product(UUID.randomUUID(), "1616", "Easybreath");
+  }
+
+  /**
+   * {@link ProductController#createProduct(Product)}
+   */
+  @Test
+  void createProduct() {
+    when(productServiceMock.createProduct(product)).thenReturn(product);
+
+    cut.createProduct(product);
+  }
+
+  @Test
+  void findProducts() {
+    when(productServiceMock.getAllProducts()).thenReturn(List(product));
+
+    assertThat(cut.findAll()).containsExactly(product);
+  }
+}
