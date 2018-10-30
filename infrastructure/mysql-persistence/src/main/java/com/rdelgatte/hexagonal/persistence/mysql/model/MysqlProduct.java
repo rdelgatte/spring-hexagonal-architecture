9a9f5e88375cba1fc9f307201d7656f53b38f183
Remi Delgatte <remi.delgatte@decathlon.com>
2018-10-30 09:20:54 +0100

    Rename packages for consistency + fix warning at startup (mysql ssl disabled)

---
+++ infrastructure/mysql-persistence/src/main/java/com/rdelgatte/hexagonal/persistence/mysql/model/MysqlProduct.java
@@ -0,0 +1,30 @@
+package com.rdelgatte.hexagonal.persistence.mysql.model;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MysqlProduct {
+
+  @Id
+  private UUID id = UUID.randomUUID();
+  private String code = "";
+  private String label = "";
+
+  public MysqlProduct fromDomain(com.rdelgatte.hexagonal.product.domain.Product product) {
+    return new MysqlProduct(product.getId(), product.getCode(), product.getLabel());
+  }
+
+  public com.rdelgatte.hexagonal.product.domain.Product toDomain() {
+    return new com.rdelgatte.hexagonal.product.domain.Product(id, code, label);
+  }
+}
---
+++ infrastructure/rest-client/src/main/java/com/rdelgatte/hexagonal/client/rest/controller/ProductController.java
@@ -0,0 +1,47 @@
+package com.rdelgatte.hexagonal.client.rest.controller;
+
+import com.rdelgatte.hexagonal.product.api.ProductService;
+import com.rdelgatte.hexagonal.product.domain.Product;
+import io.vavr.collection.List;
+import io.vavr.control.Option;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ProductController.BASE_PATH)
+@AllArgsConstructor
+public
+class ProductController {
+
+  static final String BASE_PATH = "products";
+  private static final String RESOURCE_PATH = "{code}";
+  private final ProductService productService;
+
+  @PostMapping
+  void createProduct(@RequestBody Product product) {
+    productService.createProduct(product);
+  }
+
+  @GetMapping
+  List<Product> findAll() {
+    return productService.getAllProducts();
+  }
+
+  @GetMapping(RESOURCE_PATH)
+  Option<Product> find(@PathVariable String code) {
+    return productService.findProductByCode(code);
+  }
+
+  @DeleteMapping(RESOURCE_PATH)
+  void delete(@PathVariable String code) {
+    productService.deleteProduct(code);
+  }
+
+
+}
